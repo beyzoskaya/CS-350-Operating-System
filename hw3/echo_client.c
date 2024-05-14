64a8532ac220d5210b920ddf77bd923a65e01dd9
beyzoskaya <beyza.kaya@ozu.edu.tr>
2024-05-15 00:47:52 +0300

    echoes added

--- hw3/echo_client.c
+++ hw3/echo_client.c
@@ -3,49 +3,21 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-#include <pthread.h>
-#include <stdlib.h>
-#include <locale.h>
 
 #define SERVER_ADDRESS "127.0.0.1"
 #define PORT_NUMBER 8888
 #define MAX_MESSAGE_SIZE 1000
 
-int sock;
-
-void *receive_messages(void *arg) {
-    char response[MAX_MESSAGE_SIZE];
-    ssize_t bytes_received;
-
-    while ((bytes_received = recv(sock, response, MAX_MESSAGE_SIZE - 1, 0)) > 0) {
-        response[bytes_received] = '\0'; // Null-terminate the received message
-        printf("Server: %s\n", response);
-        memset(response, 0, MAX_MESSAGE_SIZE); // Clear the message buffer
-        printf("Enter message: ");
-        fflush(stdout); // Flush output buffer to ensure prompt appears immediately
-    }
-
-    if (bytes_received == 0) {
-        printf("Server disconnected\n");
-    } else if (bytes_received == -1) {
-        perror("Error receiving data from server");
-    }
-
-    close(sock);
-    exit(EXIT_SUCCESS);
-}
-
 int main() {
-    setlocale(LC_ALL, "en_US.UTF-8");
-
+    int sock;
     struct sockaddr_in server;
-    char message[MAX_MESSAGE_SIZE];
+    char message[MAX_MESSAGE_SIZE], server_reply[MAX_MESSAGE_SIZE];
 
     // Create socket
     sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock == -1) {
         perror("Could not create socket");
-        exit(EXIT_FAILURE);
+        return 1;
     }
 
     // Prepare server address structure
@@ -54,28 +26,35 @@ int main() {
     server.sin_port = htons(PORT_NUMBER);
 
     // Connect to server
-    if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0) {
+    if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0) {
         perror("Connection failed");
-        exit(EXIT_FAILURE);
+        return 1;
     }
 
     printf("Connected to server\n");
 
-    // Create thread to receive messages
-    pthread_t receiver_thread;
-    if (pthread_create(&receiver_thread, NULL, receive_messages, NULL) < 0) {
-        perror("Could not create receiver thread");
-        exit(EXIT_FAILURE);
-    }
-
     // Main loop to send messages
     while (1) {
-        printf("Enter message: ");
+        printf("Enter message : ");
         fgets(message, sizeof(message), stdin);
-        send(sock, message, strlen(message), 0);
 
-        // Clear the message buffer
-        memset(message, 0, sizeof(message));
+        // Send some data
+        if (send(sock, message, strlen(message), 0) < 0) {
+            puts("Send failed");
+            return 1;
+        }
+
+        // Receive a reply from the server
+        if (recv(sock, server_reply, MAX_MESSAGE_SIZE, 0) < 0) {
+            puts("recv failed");
+            break;
+        }
+
+        // Print both client's message and server's echo on the same line
+        printf("Client: %sServer echoes: %s", message, server_reply);
+
+        memset(message, 0, sizeof(message)); // Clear input buffer
+        memset(server_reply, 0, sizeof(server_reply)); // Clear server reply buffer
     }
 
     close(sock);
--- hw3/echo_server.c
+++ hw3/echo_server.c
@@ -6,10 +6,18 @@
 #include <sys/socket.h>
 #include <pthread.h>
 #include <locale.h>
+#include <signal.h>
 
 #define PORT_NUMBER 8888
 #define BUFFER_SIZE 1024
+#define MAX_CLIENTS 10
 
+// Global variables
+int server_socket;
+int client_sockets[MAX_CLIENTS];
+int num_clients = 0;
+
+// Function to handle client connections
 void *handle_client_connection(void *arg) {
     int client_socket = *((int *)arg);
     char buffer[BUFFER_SIZE];
@@ -24,24 +32,60 @@ void *handle_client_connection(void *arg) {
         printf("Client connected: Unknown address\n");
     }
 
+    // Add client socket to the array
+    client_sockets[num_clients++] = client_socket;
+
     while ((bytes_received = recv(client_socket, buffer, BUFFER_SIZE, 0)) > 0) {
+        buffer[bytes_received] = '\0'; // Null-terminate the received message
+        printf("Client %s:%d says: %s", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port), buffer);
+
+        // Echo back to the client
         send(client_socket, buffer, bytes_received, 0);
-        memset(buffer, 0, BUFFER_SIZE);
+        printf("Server echoes: %s", buffer);
+
+        memset(buffer, 0, BUFFER_SIZE); // Clear buffer for next message
     }
 
+    // Client disconnected
     if (bytes_received == 0) {
-        printf("Client disconnected: %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+        printf("Client %s:%d disconnected\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
     } else if (bytes_received == -1) {
         perror("Error receiving data from client");
+    }
+
+    // Remove client socket from the array
+    for (int i = 0; i < num_clients; i++) {
+        if (client_sockets[i] == client_socket) {
+            for (int j = i; j < num_clients - 1; j++) {
+                client_sockets[j] = client_sockets[j + 1];
+            }
+            num_clients--;
+            break;
+        }
     }
 
     close(client_socket);
     pthread_exit(NULL);
 }
 
+// Signal handler function for SIGINT (Ctrl+C)
+void sigint_handler(int signum) {
+    printf("\nServer shutting down...\n");
+
+    // Close all client sockets
+    for (int i = 0; i < num_clients; i++) {
+        close(client_sockets[i]);
+    }
+
+    // Close the server socket
+    close(server_socket);
+
+    // Exit the program
+    exit(EXIT_SUCCESS);
+}
+
 int main() {
     setlocale(LC_ALL, "en_US.UTF-8");
-    int server_socket, client_socket;
     struct sockaddr_in server_addr, client_addr;
     socklen_t addr_size;
 
@@ -50,6 +94,8 @@ int main() {
     if (server_socket == -1) {
         perror("Socket creation failed");
         exit(EXIT_FAILURE);
+    } else {
+        printf("Socket created successfully\n");
     }
 
     // Prepare the server address structure
@@ -61,11 +107,25 @@ int main() {
     if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
         perror("Binding failed");
         exit(EXIT_FAILURE);
+    } else {
+        printf("Socket binding successful\n");
     }
 
     // Listen for incoming connections
     if (listen(server_socket, 5) == -1) {
         perror("Listening failed");
+        exit(EXIT_FAILURE);
+    } else {
+        printf("Listening for incoming connections...\n");
+    }
+
+    // Set up signal handler for SIGINT
+    struct sigaction sa;
+    sa.sa_handler = sigint_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    if (sigaction(SIGINT, &sa, NULL) == -1) {
+        perror("Error setting up SIGINT handler");
         exit(EXIT_FAILURE);
     }
 
@@ -74,7 +134,7 @@ int main() {
     while (1) {
         // Accept incoming connection
         addr_size = sizeof(client_addr);
-        client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &addr_size);
+        int client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &addr_size);
         if (client_socket == -1) {
             perror("Accepting client connection failed");
             continue;
@@ -92,6 +152,7 @@ int main() {
         pthread_detach(client_thread);
     }
 
+    // Close the server socket (this should not be reached)
     close(server_socket);
     return 0;
 }
